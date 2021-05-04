56d4536f96b27f61fe6ba2b36c5688c97830d1bb
maoning <ning.mao@mail.utoronto.ca>
2021-05-03 22:50:08 -0700

    added internal request deadline inside vulnapp and lots of error handlings, resolves #1 #2

--- vulnapp/app.py
+++ vulnapp/app.py
@@ -2,13 +2,18 @@ from flask import Flask, abort, render_template, redirect, request, url_for
 import requests
 from os import path, environ
 
+DEADLINE = 6
 
 app = Flask(__name__)
 app._static_folder = path.abspath('static/')
 
 @app.errorhandler(403)
-def page_not_found(e):
-    return render_template('error.html'), 403
+def forbbiden(e):
+    return render_template('error.html', error=str(e)), 403
+
+@app.errorhandler(500)
+def internal_error(e):
+    return render_template('error.html', error=str(e)), 500
 
 @app.route('/')
 def index():
@@ -25,11 +30,18 @@ def message():
     try:
         url = 'http://' + environ.get('XSSBOT_HOST') + ':5555'
         app.logger.info('make request to %s', url)
-        requests.get(url, params = {'message': message})
+        response = requests.get(url, params = {'message': message}, timeout=DEADLINE)
+        if (response.status_code < 400):
+            return redirect(url_for('submission_result', message=message))
+        else:
+            abort(500, description=response.text)
+    except requests.exceptions.Timeout:
+        app.logger.info('xssbot failed to return a response in %ds', DEADLINE)
+        abort(500, description='It took admin too long to read the message, admin gave up :(')
     except Exception as e:
-        app.logger.info(e)
-    finally:
-        return redirect(url_for('submission_result', message=message))
+        app.logger.error('unexpected exception encountered when calling xssbot')
+        abort(500, description='Unexpected expected exception encountered')
+
 
 @app.route('/admin', methods=['GET'])
 def admin():
@@ -40,4 +52,4 @@ def admin():
         abort(403, description='Only admin can see this page')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=False, host='0.0.0.0')
--- xssbot/app.js
+++ xssbot/app.js
@@ -16,47 +16,47 @@ const validUrl = (s) => {
 app.get('/', async (req, res) => {
     const {message} = req.query;
     if (!message || message.length === 0) {
-        return res.json({error: 'message query parameter is required'});
+        return res.send('message query parameter is required');
     }
     if (!validUrl(message)) {
-      return res.json({error: 'message is not a valid url, read message'});
+      return res.send('message is not a valid url, read message');
     }
 
     console.log("reading message: " + message);
-    const status = await visit(message);
-    return status == 0 ?
-      res.json({success: 'message is a valid url, visited url.'}) :
-      res.json({failed: 'message is a valid url, visited url but encountered error.'});
+    try {
+      const status = await visit(message);
+      return res.send(`message is a valid url, visited url with status code [${status}].`);
+    } catch (error) {
+      console.log(error);
+      // TODO: figure out how to import TimeoutError here to do type checking.
+      return res.status(500).send('message is a valid url, visited url but encountered an unexpected error, likely timed out.');
+    }
 });
 
 app.listen(process.env.PORT || 5555);
 
 async function visit(url) {
-   const browser = await puppeteer.launch({
-       headless: true,
-       executablePath: '/usr/bin/chromium-browser',
-       args: [
-       "--no-sandbox",
-       "--disable-gpu",
-       ]
-   });
+  const browser = await puppeteer.launch({
+     headless: true,
+     executablePath: '/usr/bin/chromium-browser',
+     args: [
+     "--no-sandbox",
+     "--disable-gpu",
+     ]
+  });
 
-    const page = await browser.newPage();
-    await page.setCookie({
-      'name': process.env.COOKIE_NAME || "secret-cookie",
-      'value': process.env.COOKIE_VALUE || "cookie-value",
-      'url': process.env.COOKIE_URL || "http://vulnapp"
-    })
-    const response = await page.goto(url, {
-      timeout: 5000,
-      waitUntil: 'networkidle0',
-    });
+  const page = await browser.newPage();
+  await page.setCookie({
+    'name': process.env.COOKIE_NAME || "secret-cookie",
+    'value': process.env.COOKIE_VALUE || "cookie-value",
+    'url': process.env.COOKIE_URL || "http://vulnapp"
+  })
+  const response = await page.goto(url, {
+    timeout: 5000,
+    waitUntil: 'networkidle0',
+  });
 
-    await page.close();
-    await browser.close();
-
-  if (response._status < 400) {
-    return 0;
-  }
-  return 1;
+  await page.close();
+  await browser.close();
+  return response._status;
 }
