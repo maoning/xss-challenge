22c99f423d6a7c21f7ad30d9fd1c24eeed47f827
maoning <ning.mao@mail.utoronto.ca>
2021-05-08 10:42:41 -0700

    Create a standalone flask app with xss vulnerability

--- README.md
+++ README.md
@@ -1,22 +1,4 @@
-# xss-challenge
-An xss web challenge based on flask, nodejs, headless chrome &amp; puppeteer
+# vulnapp
 
-## Local Setup
-```
-git clone https://github.com/maoning/xss-challenge.git
-cd xss-challenge
-docker-compose build
-docker-compose up
-```
-
+A simple flask app that contains a reflected xss vulnerability.
 Visit `http://localhost:5000` to access the vulnerable app.
-
-## Design
-### vulnapp
-vulnapp container hosts a simple flask app that contains a reflected xss vulnerability. When a message is submitted via the app, it calls xssbot service to read the message.
-
-### xssbot
-xssbot is a nodejs server. Once it receives an api call from vulnapp to read a message, and if the message is a url, it will use headless chrome to visit that url with an admin cookie.
-
-## Caveat
-xssbot can reach vulnapp service via http://vulnapp:5000.
--- vulnapp/app.py
+++ vulnapp/app.py
@@ -1,19 +1,9 @@
-from flask import Flask, abort, render_template, redirect, request, url_for
+from flask import Flask, render_template, redirect, request, url_for
 import requests
-from os import path, environ
-
-DEADLINE = 6
+from os import path
 
 app = Flask(__name__)
 app._static_folder = path.abspath('static/')
-
-@app.errorhandler(403)
-def forbbiden(e):
-    return render_template('error.html', error=str(e)), 403
-
-@app.errorhandler(500)
-def internal_error(e):
-    return render_template('error.html', error=str(e)), 500
 
 @app.route('/')
 def index():
@@ -27,29 +17,8 @@ def submission_result():
 @app.route('/message', methods=['POST'])
 def message():
     message = request.form.get('message')
-    try:
-        url = 'http://' + environ.get('XSSBOT_HOST') + ':5555'
-        app.logger.info('make request to %s', url)
-        response = requests.get(url, params = {'message': message}, timeout=DEADLINE)
-        if (response.status_code < 400):
-            return redirect(url_for('submission_result', message=message))
-        else:
-            abort(500, description=response.text)
-    except requests.exceptions.Timeout:
-        app.logger.info('xssbot failed to return a response in %ds', DEADLINE)
-        abort(500, description='It took admin too long to read the message, admin gave up :(')
-    except Exception as e:
-        app.logger.error('unexpected exception encountered when calling xssbot')
-        abort(500, description='Unexpected expected exception encountered')
+    return redirect(url_for('submission_result', message=message))
 
-
-@app.route('/admin', methods=['GET'])
-def admin():
-    secret = request.cookies.get(environ.get('COOKIE_NAME'))
-    if secret == environ.get('COOKIE_VALUE'):
-        return render_template('admin.html', flag = environ.get('FLAG'))
-    else:
-        abort(403, description='Only admin can see this page')
 
 if __name__ == '__main__':
     app.run(debug=False, host='0.0.0.0')
